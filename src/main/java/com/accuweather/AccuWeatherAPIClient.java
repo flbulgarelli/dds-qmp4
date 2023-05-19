708d7a2b8aeb3fb901585ce35a2dabf655dc34ff
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-05-19 18:39:23 -0300

    Sample implementation of accuweather

--- src/main/java/com/accuweather/AccuWeatherAPIClient.java
+++ src/main/java/com/accuweather/AccuWeatherAPIClient.java
@@ -9,11 +9,18 @@ public class AccuWeatherAPIClient implements AccuWeatherAPI {
 
   private Client client = ClientBuilder.newClient();
 
+  /**
+   * @param location
+   * @return
+   * @see https://developer.accuweather.com/accuweather-current-conditions-api/apis
+   */
   @Override
   public Map<String, Object> getWeather(String location) {
-    return this.client.target("https://accuweather.com")
-            .path("/climate")
+    return this.client.target("http://dataservice.accuweather.com/currentconditions/v1")
+            .path(location)
+            .queryParam("apiKey", "sacar esto de una variable de entorno o un archivo de configuracion")
             .request(MediaType.APPLICATION_JSON)
             .get(Map.class);
+
   }
 }
