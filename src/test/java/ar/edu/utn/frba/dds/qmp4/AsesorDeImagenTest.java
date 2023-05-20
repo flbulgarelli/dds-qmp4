359f238b7a110bb653994efe860a440086bad78f
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-05-19 22:48:12 -0300

    Implement first real test

--- src/test/java/ar/edu/utn/frba/dds/qmp4/AsesorDeImagenTest.java
+++ src/test/java/ar/edu/utn/frba/dds/qmp4/AsesorDeImagenTest.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frba.dds.qmp4;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -8,9 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class AsesorDeImagenTest {
 
   @Test
-  void sePuedenGenerarAtuendosParaElClimaActual() {
+  void sePuedenGenerarAtuendosParaBuenosAiresCon18GradosY100PorcientoDeHumedad() {
     ServicioMeteorologico servicio = Mockito.mock(ServicioMeteorologico.class);
+    
+    Mockito
+            .when(servicio.obtenerCondicionesClimaticas("Buenos Aires"))
+            .thenReturn(new EstadoDelTiempo(18, Humedad.ALTA));
+
+    var guardarropas = new Guardarropas();
     var asesor = new AsesorDeImagen(servicio);
+
+    Assertions.assertNotNull(asesor.sugerirAtuendo("Buenos Aires", guardarropas));
   }
 
 }
